f9634b3ed0d9b12ce7fd5097ece7e92e9fdb874f
Kevin Chen <kevinch28@gmail.com>
2022-03-02 10:47:07 -0500

    adjusted text on start screen

--- src/scripts/start_screen.js
+++ src/scripts/start_screen.js
@@ -16,12 +16,20 @@ class StartScreen {
     
     this.ctx.fillStyle = 'black';
     this.ctx.font = '20px arial';
-    this.ctx.fillText('Monsters have invaded the peaceful realm of Mekraine and have made their way', 100, 150);
-    this.ctx.fillText('to your village. Pick up your weapons and protect your home!', 100, 175);
+    this.ctx.fillText('Monsters have invaded the peaceful realm', 270, 125);
+    this.ctx.fillText('of Mekraine and have made their way to your', 255, 150);
+    this.ctx.fillText('village. Pick up your weapons and protect your home!', 220, 175);
     this.ctx.fillText('Place defenders on the screen, by clicking on the screen.', 100, 250);
-    this.ctx.fillText('Keep an eye on your resources, each defender consumes resources.', 100, 300);
-    this.ctx.fillText('Defeating monsters will acquire resources.', 100, 350);
+    this.ctx.fillText('Keep an eye on your resources, each defender consumes 100 resource.', 100, 300);
+    this.ctx.fillText('Defeating monsters will acquire 50 resource.', 100, 350);
+
+    this.ctx.fillStyle = 'yellow'
+    this.ctx.font = '25px arial';
     this.ctx.fillText('CLICK TO START', 350, 450);
+
+    this.ctx.fillStyle = 'black'
+    this.ctx.font = '15px arial';
+    this.ctx.fillText('*no relations to real life events/places', 640, 585)
 
     canvas.addEventListener('click', function() {
 
